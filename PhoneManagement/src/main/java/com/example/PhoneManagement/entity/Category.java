548899b2f6badea69f0ba69061f75ed8251993ed
frot77 <mihtuan199xxx@gmail.com>
2024-07-16 00:18:48 +0700

    update change entity package

--- PhoneManagement/src/main/java/com/example/PhoneManagement/entity/Category.java
+++ PhoneManagement/src/main/java/com/example/PhoneManagement/entity/Category.java
@@ -2,13 +2,17 @@ package com.example.PhoneManagement.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 
-@Data
 @Entity(name = "Category")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Category {
     @Id
@@ -16,7 +20,7 @@ public class Category {
     @Column(name="category_id")
     int categoryId;
 
-    @Column(name = "category_name")
+    @Column(name = "category_name", nullable = false, length = 100)
     String categoryName;
 
     @OneToMany(mappedBy = "category")
