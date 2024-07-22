5cfc396ad0658f572ca83042b7cd11d183b2a96a
quanphan209 <127081989+quanphan2k3@users.noreply.github.com>
2024-07-22 20:30:44 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,15 +1,14 @@
-
 CREATE DATABASE PhoneManagement;
 USE PhoneManagement;
 
 -- Table to manage user roles
-CREATE TABLE Role (
+CREATE TABLE role (
     role_id INT AUTO_INCREMENT PRIMARY KEY,
     role_name VARCHAR(50) NOT NULL
 );
 
 -- Table to manage user accounts
-CREATE TABLE UserAccount (
+CREATE TABLE useraccount (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
@@ -18,19 +17,19 @@ CREATE TABLE UserAccount (
     phone_number VARCHAR(50) NOT NULL,
     role_id INT,
     active BIT,
-    avatar longtext, 
+    avatar LONGTEXT, 
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (role_id) REFERENCES Role(role_id)
+    FOREIGN KEY (role_id) REFERENCES role(role_id)
 );
 
 -- Table to manage product categories
-CREATE TABLE Category (
+CREATE TABLE category (
     category_id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(100) NOT NULL
 );
 
 -- Table to manage products
-CREATE TABLE Product (
+CREATE TABLE product (
     product_id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(100) NOT NULL,
     description TEXT,
@@ -39,51 +38,52 @@ CREATE TABLE Product (
     price DECIMAL(10,2) NOT NULL,
     warranty_period INT, -- Warranty period (months)
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (category_id) REFERENCES Category(category_id)
+    FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
 
 -- Table to manage colors
-CREATE TABLE Color (
+CREATE TABLE color (
     color_id INT AUTO_INCREMENT PRIMARY KEY,
     color_name VARCHAR(50) NOT NULL
 );
 
 -- Junction table to manage many-to-many relationship between products and colors
-CREATE TABLE ProductColor (
+CREATE TABLE productcolor (
+	product_color_id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT,
     image TEXT,
     color_id INT,
     quantity INT,
     last_updated DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (product_id, color_id),
-    FOREIGN KEY (product_id) REFERENCES Product(product_id),
-    FOREIGN KEY (color_id) REFERENCES Color(color_id)
+--     PRIMARY KEY (product_id, color_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    FOREIGN KEY (color_id) REFERENCES color(color_id)
 );
 
 -- Table to manage orders
-CREATE TABLE Orders (
+CREATE TABLE orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,  -- nguoi mua
     saler_id INT, -- nguoi ban
     order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     total_amount DECIMAL(10,2),
     status VARCHAR(50), 
-    FOREIGN KEY (user_id) REFERENCES UserAccount(user_id)
+    FOREIGN KEY (user_id) REFERENCES useraccount(user_id)
 );
 
 -- Table to manage order details
-CREATE TABLE OrderDetail (
+CREATE TABLE orderdetail (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
-    product_id INT,
+    product_color_id INT,
     quantity INT NOT NULL,
     price DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES Product(product_id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_color_id) REFERENCES productcolor(product_color_id)
 );
 
 -- Table to manage warranty and repair
-CREATE TABLE WarrantyRepair (
+CREATE TABLE warrantyrepair (
     warranty_id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT,
     user_id INT,   -- Nguoi can sua
@@ -91,28 +91,28 @@ CREATE TABLE WarrantyRepair (
     issue_description VARCHAR(255),
     repair_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(50),
-    FOREIGN KEY (product_id) REFERENCES Product(product_id),
-    FOREIGN KEY (user_id) REFERENCES UserAccount(user_id)
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    FOREIGN KEY (user_id) REFERENCES useraccount(user_id)
 );  
 
 -- Table to manage purchases (hàng nhập)
-CREATE TABLE Purchase (
+CREATE TABLE purchase (
     purchase_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,  -- Nguoi nhap hang
     purchase_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     total_amount DECIMAL(10,2),
     origin VARCHAR(50),
     status VARCHAR(50),
-    FOREIGN KEY (user_id) REFERENCES UserAccount(user_id)
+    FOREIGN KEY (user_id) REFERENCES useraccount(user_id)
 );
 
 -- Table to manage purchase details (chi tiết hàng nhập)
-CREATE TABLE PurchaseDetail (
+CREATE TABLE purchasedetail (
     purchase_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     purchase_id INT,
     product_id INT,
     quantity INT NOT NULL,
     price DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (purchase_id) REFERENCES Purchase(purchase_id),
-    FOREIGN KEY (product_id) REFERENCES Product(product_id)
+    FOREIGN KEY (purchase_id) REFERENCES purchase(purchase_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
