28dd236cf4224bd531acb908a797815ee6218c39
frot77 <127224025+frot77@users.noreply.github.com>
2024-07-22 23:17:20 +0700

    Update length password db.sql

--- db.sql
+++ db.sql
@@ -11,7 +11,7 @@ CREATE TABLE role (
 CREATE TABLE useraccount (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     fullname VARCHAR(50) NOT NULL,
     address VARCHAR(50) NOT NULL,
     phone_number VARCHAR(50) NOT NULL,
