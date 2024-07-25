dab3edc115a331eabe5dc0149342d4b556b4e8ff
Dinhnamub2003 <115776189+Dinhnamub2003@users.noreply.github.com>
2024-07-25 21:16:21 +0700

    commit account manage

---
+++ PhoneManagement/src/main/java/com/example/PhoneManagement/dto/request/PageDTO.java
@@ -0,0 +1,18 @@
+package com.example.PhoneManagement.dto.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PageDTO<T> {
+
+    Integer PageSize;
+
+    Integer PageNumber;
+
+    public PageDTO(int currentPage, int pageSize) {
+        this.PageNumber = currentPage;
+        this.PageSize = pageSize;
+    }
+}
