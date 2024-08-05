f60af2b62977047184525544681e62978f0a5980
quanphan2k3 <quanpahe170260@fpt.edu.vn>
2024-08-05 23:06:39 +0700

    manage category

---
+++ PhoneManagement/src/main/java/com/example/PhoneManagement/controller/CategoryController.java
@@ -0,0 +1,67 @@
+package com.example.PhoneManagement.controller;
+
+import com.example.PhoneManagement.dto.request.CategoryDTO;
+import com.example.PhoneManagement.entity.Category;
+import com.example.PhoneManagement.entity.Users;
+import com.example.PhoneManagement.service.imp.CategoryService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/category")
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+public class CategoryController {
+    CategoryService categoryService;
+    CategoryExcelImportController categoryExcelImportController;
+
+    @GetMapping
+    public String getCategory(Model model){
+        model.addAttribute("cates", categoryService.findAllCategory());
+        return "category";
+    }
+
+    @PostMapping()
+    public String addCategory(@RequestParam("name") String name){
+        CategoryDTO categoryDTO=new CategoryDTO();
+        categoryDTO.setCateName(name);
+        categoryService.addCategory(categoryDTO);
+        return "redirect:/admin/category";
+    }
+
+    @PostMapping("/{cateId}")
+    public String editCategory(@RequestParam("categoryName") String name, @PathVariable("cateId") int cateId){
+        CategoryDTO categoryDTO=new CategoryDTO();
+        categoryDTO.setCateId(cateId);
+        categoryDTO.setCateName(name);
+        categoryService.editCategory(cateId,categoryDTO);
+        return "redirect:/admin/category";
+    }
+
+    @PostMapping("/delete")
+    public String deleteCategory(@RequestParam("cateId") int cateId){
+        categoryService.deleteCategory(cateId);
+        return "redirect:/admin/category";
+    }
+
+    @PostMapping("/importCate")
+    public String importCategory(@RequestParam("file") MultipartFile file){
+        try {
+            List<Category> usersList = categoryExcelImportController.importCategoryFromExcel(file.getInputStream());
+            categoryService.saveAll(usersList);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        return "redirect:/admin/category";
+    }
+
+}
---
+++ PhoneManagement/src/main/java/com/example/PhoneManagement/controller/CategoryExcelImportController.java
@@ -0,0 +1,52 @@
+package com.example.PhoneManagement.controller;
+
+import com.example.PhoneManagement.entity.Category;
+import com.example.PhoneManagement.entity.Roles;
+import com.example.PhoneManagement.entity.Users;
+import com.example.PhoneManagement.service.imp.CategoryService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+@Controller
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class CategoryExcelImportController {
+    CategoryService categoryService;
+
+    public List<Category> importCategoryFromExcel(InputStream inputStream) throws IOException {
+        List<Category> categoryList = new ArrayList<>();
+        Workbook workbook = new XSSFWorkbook(inputStream);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+            Row row = sheet.getRow(i);
+            if (row == null) continue;
+
+            Category category = new Category();
+
+            if (row.getCell(0) != null) {
+                category.setCategoryName(row.getCell(0).getStringCellValue());
+            }
+
+            if (row.getCell(1) != null && row.getCell(1).getCellType() == CellType.BOOLEAN) {
+                category.setDeleted(row.getCell(1).getBooleanCellValue());
+            }
+
+            categoryList.add(category);
+        }
+        workbook.close();
+        return categoryList;
+    }
+
+}
