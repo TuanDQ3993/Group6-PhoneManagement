43bc89e7cb613205a7b3e5927b9b8eb60824c541
frot77 <127224025+frot77@users.noreply.github.com>
2024-08-05 20:57:13 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -15,7 +15,7 @@ CREATE TABLE useraccount
     username     VARCHAR(50)  NOT NULL,
     password     VARCHAR(255) NOT NULL,
     fullname     VARCHAR(50)  NOT NULL,
-    address      VARCHAR(50)  NOT NULL,
+    address      VARCHAR(255)  NOT NULL,
     phone_number VARCHAR(50)  NOT NULL,
     role_id      INT,
     active       BIT,
